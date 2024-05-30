76651044974c12d4397e575ba3e996249f05927e
ayushgit12 <ayush2005raj@gmail.com>
2024-05-30 10:46:34 +0530

    chore: Update TweetCard component to display formatted date and time

--- frontend/src/managers/TweetCard.jsx
+++ frontend/src/managers/TweetCard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 function TweetCard({tweet}) {
 
+  const date = new Date(tweet.createdAt);
+
 
 
   return (
@@ -10,7 +12,8 @@ function TweetCard({tweet}) {
           <div className={`bg-white-600 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2`}>
                <div className="text-gray-900 font-bold text-xl mb-2">{tweet.tweet}</div>
                <div className="text-gray-900 font-bold text-xl mb-2 text-end pr-10">-{tweet.user.fullName}</div>
-               <div className="text-gray-700 text-base">{tweet.createdAt}</div>
+               <div className="text-gray-700 text-base">Posted At {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}</div>
+               <div className="text-gray-700 text-base">Dated: {date.getDate()}/{date.getMonth()}/{date.getFullYear()}</div>
                </div>
     </div>
   )
