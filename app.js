3209271d09811ce53e7861abe41c2588603f6edf
ayushgit12 <ayush2005raj@gmail.com>
2024-06-16 09:23:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+import express from 'express'
+import { urlencoded } from 'express'
+import cors from 'cors'
+import userRouter from './routes/user.routes.js'
+import cookieParser from 'cookie-parser'
+import { Server } from 'socket.io'
+import http from 'http'
+
+
+
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  
+  }
+
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected ' + socket.id);
+  // socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
+
+  socket.on("join_room", (data,room) => {
+    socket.join(room);
+    socket.to(room).emit("join_room_data", data);
+    console.log(data + " joined " + room);
+  }
+  )
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+
+  })
+
+  socket.on("leave_room", (data, room) => {
+    socket.to(room).emit("leave_room", data);
+    console.log(data + " left " + room);
+    socket.leave(room);
+  })
+})
+server.listen(3001, () => {
+  console.log('listening on *:8000');
+})
+
+
+app.use(cors({
+     origin: "*",
+     credentials: true
+}));
+
+app.use(express.json({
+  limit: '16kb'
+}));
+
+app.use(urlencoded({extends:true, limit: '16kb'})); 
+app.use(cookieParser());
+
+// const port = 8000
+
+
+app.use('/api/v1/users', userRouter);
+
+export {app};
---
+++ routes/user.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { loginUser, logoutUser, registerUser, showTweet } from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import { postTweet } from "../controllers/user.controllers.js";
+import { deleteTweet } from "../controllers/user.controllers.js";
+import { likeTweet } from "../controllers/user.controllers.js";
+import { showLikeTweet } from "../controllers/user.controllers.js";
+import { getUserDetails } from "../controllers/user.controllers.js";
+import { changePassword } from "../controllers/user.controllers.js";
+import { sendEmail } from "../controllers/email.controllers.js";
+import { commentAdded } from "../controllers/user.controllers.js";
+import { getComments } from "../controllers/user.controllers.js";
+import { forgotPassword } from "../controllers/user.controllers.js";
+import { emailToUserID } from "../controllers/user.controllers.js";
+import { getAllUsers } from "../controllers/user.controllers.js";
+
+
+const router = Router();
+// console.log("user.routes.js");
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').post(verifyJWT,logoutUser);
+router.route('/postTweet').post(verifyJWT,postTweet);
+router.route('/showTweets').post(showTweet)
+router.route('/deleteTweet').delete(deleteTweet);
+router.route('/likeTweet').post(likeTweet);
+router.route('/showLikeTweet').post(showLikeTweet);
+router.route('/getUserDetails').post(getUserDetails);
+router.route('/changePassword').post(verifyJWT,changePassword);
+router.route('/sendEmail').post(sendEmail);
+router.route('/commentAdded').post(commentAdded);
+router.route('/getComments').post(getComments);
+router.route('/forgotPassword').post(forgotPassword);
+router.route('/emailToUserID').post(emailToUserID);
+router.route('/getAllUsers').get(getAllUsers);
+
+
+
+export default router;
